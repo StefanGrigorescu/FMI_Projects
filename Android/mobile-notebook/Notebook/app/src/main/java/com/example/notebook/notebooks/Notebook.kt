41055016433d09ae0f6e999cf3134417f9867d4f
Stefan Grigorescu <grigorescu2001@gmail.com>
2026-09-11 18:31:38 +0300

    Removed: Older version of the Android Notebook App

--- Android/mobile-notebook/Notebook/app/src/main/java/com/example/notebook/notebooks/Notebook.kt
+++
@@ -1,8 +0,0 @@
-package com.example.notebook.notebooks
-
-class Notebook(
-    var id: Long? = null,
-    var title: String = "",
-    var description: String = "",
-    var hasPassword: Boolean = false
-)
--- Android/mobile-notebook/Notebook/app/src/main/java/com/example/notebook/notes/Note.kt
+++
@@ -1,6 +0,0 @@
-package com.example.notebook.notes
-
-class Note(
-    var id: Long? = null,
-    var title: String = "",
-)
